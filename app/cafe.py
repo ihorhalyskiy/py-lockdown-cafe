3fc61b871c5f60020119f23a42f3421f40b2ba27
Ihor Halytskiy <galytskiyihor@gmail.com>
2024-12-14 12:20:36 +0200

    Fix flake8 errors change

--- app/cafe.py
+++ app/cafe.py
@@ -10,17 +10,17 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise errors.NotVaccinatedError(
-                f"{visitor.get("name")} not vaccinated!"
+                f"{visitor.get('name')} not vaccinated!"
             )
 
         if visitor["vaccine"].get("expiration_date") < date.today():
             raise errors.OutdatedVaccineError(
-                f"Vaccine of {visitor.get("name")} is expired!"
+                f"Vaccine of {visitor.get('name')} is expired!"
             )
 
         if not visitor.get("wearing_a_mask", False):
             raise errors.NotWearingMaskError(
-                f"{visitor.get("name")} "
+                f"{visitor.get('name')} "
                 "must wear a medical mask to visit!"
             )
 
